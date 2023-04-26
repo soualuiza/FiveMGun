fad9a644e70f7c8eb1bc89862041ed399ca92f57
Luiza Luna Machado <108479144+soualuiza@users.noreply.github.com>
2023-04-26 18:30:08 -0300

    Update client.js

--- Arma/client.js
+++ Arma/client.js
@@ -1,12 +1,13 @@
 
-
+//RayPistol
 RegisterCommand('dararma', ()=>{
 
     GiveWeaponToPed(PlayerPedId(), -1355376991, 100, false, true);
 
 })
 
-RegisterCommand('smg', ()=> {
+//Ak-47
+RegisterCommand('ak', ()=> {
 
     GiveWeaponToPed(PlayerPedId(), 0x394F415C, 500, false, false)
 })
