f0f324c98027cad5077ee60ee7322bed4d36616b
Luiza Luna Machado <108479144+soualuiza@users.noreply.github.com>
2023-04-26 18:12:55 -0300

    by Luna

---
+++ Arma/client.js
@@ -0,0 +1,13 @@
+
+
+RegisterCommand('dararma', ()=>{
+
+    GiveWeaponToPed(PlayerPedId(), -1355376991, 100, false, true);
+
+})
+
+RegisterCommand('smg', ()=> {
+
+    GiveWeaponToPed(PlayerPedId(), 0x394F415C, 500, false, false)
+})
+
